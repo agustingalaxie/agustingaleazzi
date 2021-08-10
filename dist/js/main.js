2ba20ea4e9913c9da3f1ef1a2c4effd0b7ae784e
Agustín Galeazzi <71106735+agustingalaxie@users.noreply.github.com>
2021-08-10 11:26:02 -0300

    Add files via upload

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+const menuBtn = document.querySelector('.menu-btn');
+const hamburguer = document.querySelector('.menu-btn__burguer');
+const nav = document.querySelector('.nav');
+const menunav = document.querySelector('.menu-nav');
+const menuitems = document.querySelectorAll('.menu-nav__item')
+
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu){
+        hamburguer.classList.add('open');
+        nav.classList.add('open');
+        menunav.classList.add('open');
+        menuitems.forEach(item => item.classList.add('open'));
+        showMenu=true;
+    } else {
+        hamburguer.classList.remove('open');
+        nav.classList.remove('open');
+        menunav.classList.remove('open');
+        menuitems.forEach(item => item.classList.remove('open'));
+        showMenu=false;
+    }
+
+}
